e67e0759713560fee11cdb6f5bd73476431d7ea7
V <sss.5151@mail.ru>
2021-10-09 13:35:08 +0400

    Social NetWork begin creaate(ReactJS)

---
+++ src/components/Profile.jsx
@@ -0,0 +1,36 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+       <div>
+        <img src='https://images.ctfassets.net/hrltx12pl8hq/1XAMCcfZRbFQmNCuFR8oXH/e1bddb571332e355b076109938db0bf3/shutterstock_1308690499.jpg?fit=fill&w=800&h=400' />
+       </div>
+       <div>
+         ava + desc
+       </div>
+
+       <div>
+         Post
+
+         <div>
+           new post
+         </div>
+
+
+         <div>
+
+           <div>
+             post 1
+           </div>
+
+           <div>
+             post 1
+           </div>
+
+         </div>
+
+       </div>
+       
+     </div>
+  )
+}
+export default Profile
