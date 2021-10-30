1b25d3ebb5aea9847819187a02f3318b8a79514a
V <sss.5151@mail.ru>
2021-10-30 16:12:46 +0400

    basename

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ let AppContainer = compose(
 
 const SocialNetworkApp = (props) => {
   return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
           <AppContainer />
         </Provider>
